ea0620aca62f500ca79b4ec04b9a4d9acc80a9bf
Jason Smith <jhs@couchone.com>
2011-02-09 11:37:26 +0700

    Initial implementation

--- cli.js
+++ cli.js
@@ -1,6 +1,37 @@
 #!/usr/bin/env node
 // The jss command-line interface.
+//
 
-var argv = require('optimist').argv;
+var sys = require('sys')
+  , jss = require('jss')
+  ;
 
-console.log("argv: " + require('sys').inspect(argv));
+var usage = 'jss <test predicate> [result expression]';
+var argv = require('optimist').usage(usage).argv
+  , predicate = argv._[0]
+  , expression = argv._[1]
+  , test = new Function('obj', 'with (obj) { return !!(' + predicate + ') }')
+  , format = function(obj) { return JSON.stringify(obj) }
+  ;
+
+if(!predicate) {
+  console.log(usage);
+  process.exit(1);
+}
+
+if(expression) {
+  var getter = new Function('obj', 'with (obj) { return (' + expression + ') }');
+  format = function(obj) {
+    var result = getter.apply(obj, [obj]);
+    if(typeof result === "object")
+      result = JSON.stringify(result);
+    return "" + result;
+  }
+}
+
+var stream = new jss.Stream();
+stream.test = test;
+stream.format = format;
+stream.in = process.openStdin();
+stream.out = process.stdout;
+stream.pump();
--- jss.js
+++ jss.js
@@ -1,7 +1,78 @@
 // The jss API
 //
 
-function Filter () {
+var sys = require('sys')
+  , events = require('events')
+  ;
+
+function Stream () {
+  var self = this;
+  events.EventEmitter.call(self);
+
+  self.test = null;
+  self.format = null;
+  self.in = null;
+  self.out = null;
+
+  self.on('line', function on_line(line) {
+    if(!self.test)
+      throw new Error("No JS test defined");
+
+    var obj;
+    try      { obj = JSON.parse(line) }
+    catch(e) { return; /* Nothing to do */ }
+
+    self.emit('json', obj);
+  })
+
+  self.on('json', function on_json(obj) {
+    var result = false;
+    try      { result = self.test.apply(obj, [obj]) }
+    catch(e) { return; }
+
+    if(result)
+      self.emit('match', obj);
+  })
+
+  self.on('match', function on_match(obj) {
+    try {
+      var output = self.format.apply(obj, [obj]);
+      if(output) {
+        self.out.write(output);
+        self.out.write("\n");
+      }
+    } catch (e) {
+      return; /* Nothing to do */
+    }
+  })
+}
+sys.inherits(Stream, events.EventEmitter);
+
+Stream.prototype.pump = function() {
+  var self = this
+    , ready_lines = []
+    , unterminated = ""
+    ;
+
+  self.in.setEncoding('utf8');
+  self.in.on('data', function on_data(chunk) {
+    chunk.split(/\n/).forEach(function(line, a, lines) {
+      if(a === 0) {
+        line = unterminated + line;
+        unterminated = "";
+      }
+
+      if(a + 1 === lines.length) {
+        unterminated = line;
+      } else {
+        ready_lines.push(line.replace(/,$/, ""));
+      }
+    })
+
+    var line;
+    while(line = ready_lines.shift())
+      self.emit('line', line);
+  })
 }
 
-exports.Filter = Filter;
+exports.Stream = Stream;
